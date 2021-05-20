2982032344c5ca9845dfabcfbb28bf05116930a1
Gleb Bukhanov <buhanka2002@gmail.com>
2021-05-20 12:23:18 +0300

    Написать программу, в которой описана иерархия классов. Геометрические фигуры(ромб, параллелепипед, эллипс). Реализовать методы вычисления площади и периметра фигуры. Продемонстрировать работу, предостапвив пользователю право выбора типа фигуры.

---
+++ heritage/heritage.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "heritage.h"
+using namespace std;
+
+double Romb::Perimeter()const { return 4 * lenth; };
+double Romb::Square()const { return l * hight; };
+
+double Parallelepiped::Perimeter()const { return 4 * lenth + 4 * width + 4 * hight; };
+double Parallelepiped::Square()const { return 2 * (lenth * width + lenth * hight + width * hight); };
+
+double Ellipse:: Perimeter()const { return 2 * M_PI * (sqrt(pow(largesemiaxis, 2) * pow(smallsemiaxis, 2)) / 2); };
+double Ellipse:: Square()const { return M_PI * largesemiaxis * smallsemiaxis; };
+
+
